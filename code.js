a7f75afedbd13d31540e4d004f2e7c5517759d86
lucasc-code <lucascohen72@gmail.com>
2022-05-29 15:54:04 -0700

    Website Portfolio page

---
+++ code.js
@@ -0,0 +1,19 @@
+function functionName() {
+    document.write("You are learning how to call Javascript functions in html");
+}
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
